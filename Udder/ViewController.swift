f4cdbc99203887f46d67de0993dbc99fd3e3160e
Sally El Mossalami <AMO@mahmouds-macbook-air.local>
2019-01-28 18:53:05 +0100

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Abdel Monem El Mossalami on 28/01/2019.
+//  Copyright © 2019 Mossalami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
